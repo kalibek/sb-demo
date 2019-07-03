6364d57c6c67c173797e4bf26df0fca970a5e6cc
Kalibek Turgumbayev <kalibek_turgumbayev@epam.com>
2019-07-03 19:29:18 +0600

    initial sb-project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import java.util.Random;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Entity
+@Table(name = "person")
+@Data
+class Person {
+
+  @Id
+  private long id;
+  private String name;
+
+}
+
+interface PersonRepo extends JpaRepository<Person, Long> {
+
+}
+
+@RestController
+@RequestMapping("api/person")
+@RequiredArgsConstructor
+class PersonResource {
+
+  private final PersonRepo personRepo;
+
+  @GetMapping
+  public ResponseEntity getList() {
+    return ResponseEntity.ok(personRepo.findAll());
+  }
+
+}
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class DemoApplication implements CommandLineRunner {
+
+  private final PersonRepo personRepo;
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    Person person = new Person();
+    person.setName("person #" + new Random().nextInt());
+    personRepo.save(person);
+  }
+}
